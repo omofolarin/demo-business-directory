f14b23677d8fb775e938779166b6a16e22f2812d
omofolarin <folarinshonibare@yahoo.com>
2019-11-08 03:07:11 +0100

    added use-localStorage hook

---
+++ src/hooks/use-localStorage.ts
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+function useLocalStorage(
+  key: string,
+  initialValue: any[] | Record<string, any> | string | null | undefined
+) {
+  const [storedValue, setStoredValue] = React.useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: any) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+export default useLocalStorage;
