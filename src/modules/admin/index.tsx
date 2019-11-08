cbf9a60ac2c3d70aeccc899f9dcd9f05fac4fa34
omofolarin <folarinshonibare@yahoo.com>
2019-11-08 03:10:10 +0100

    boiler plate files

---
+++ src/modules/admin/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Admin = () => {
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ src/modules/website/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Website = () => {
+  return (
+    <div>
+      <h1>Website</h1>
+    </div>
+  );
+};
+
+export default Website;
