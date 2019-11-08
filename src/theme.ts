25c0ac42fe981523c7387d7e34bee5ae2c135b13
omofolarin <folarinshonibare@yahoo.com>
2019-11-08 07:58:33 +0100

    added theme configs

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#322480"
+    }
+  },
+  typography: {
+    h5: {
+      color: "#322480"
+    }
+  }
+});
+
+export default theme;
