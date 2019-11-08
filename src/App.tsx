2030bf5ae7e6fbf47c315dca5bcb30344c1c7cec
omofolarin <folarinshonibare@yahoo.com>
2019-11-08 03:08:31 +0100

    bootstrap app with default data on first initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ProjectRoutes from "./routes";
+import useLocalStorage from "./hooks/use-localStorage";
+import functions from "./functions";
 
 const App: React.FC = () => {
+  const [storedValue, setValue] = useLocalStorage("businessDirectory", {});
+
+  const initialize = () => {
+    const { onBootstrap, isBootstrapped } = functions.app(storedValue);
+    if (!isBootstrapped()) {
+      onBootstrap(setValue);
+    }
+  };
+
+  React.useEffect(() => {
+    initialize();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ProjectRoutes />
+    </Router>
   );
-}
+};
 
 export default App;
