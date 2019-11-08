3e5e05be13312fedda93dd19721170a2c633fd58
omofolarin <folarinshonibare@yahoo.com>
2019-11-08 07:58:50 +0100

    added login UI

---
+++ src/modules/admin/pages/login/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  FormControl,
+  InputLabel,
+  TextField,
+  Button
+} from "@material-ui/core";
+
+interface Props {}
+
+export default function Login({  }: Props): any {
+  return (
+    <Container maxWidth="sm">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh"
+        }}
+      >
+        <Card style={{ minWidth: "80%", height: "40vh" }}>
+          <CardHeader title="Login" style={{ textAlign: "center" }} />
+          <CardContent>
+            <TextField
+              name="email"
+              label="Email"
+              margin="normal"
+              variant="outlined"
+              style={{ width: "100%" }}
+            />
+            <TextField
+              name="password"
+              label="Password"
+              margin="normal"
+              variant="outlined"
+              style={{ width: "100%" }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ width: "100%", height: "3.5em", boxShadow: "none" }}
+            >
+              Submit
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </Container>
+  );
+}
