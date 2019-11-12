5f11afee2567568b36994bd828010a6adf0dc948
omofolarin <folarinshonibare@yahoo.com>
2019-11-12 09:26:14 +0100

    using react-select

---
+++ src/components/categories-input/index.tsx
@@ -0,0 +1,283 @@
+import React, { CSSProperties, HTMLAttributes } from "react";
+import clsx from "clsx";
+import Select from "react-select";
+import {
+  createStyles,
+  emphasize,
+  makeStyles,
+  useTheme,
+  Theme
+} from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import NoSsr from "@material-ui/core/NoSsr";
+import TextField, { BaseTextFieldProps } from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Chip from "@material-ui/core/Chip";
+import MenuItem from "@material-ui/core/MenuItem";
+import CancelIcon from "@material-ui/icons/Cancel";
+import { ValueContainerProps } from "react-select/src/components/containers";
+import { ControlProps } from "react-select/src/components/Control";
+import { MenuProps, NoticeProps } from "react-select/src/components/Menu";
+import { MultiValueProps } from "react-select/src/components/MultiValue";
+import { OptionProps } from "react-select/src/components/Option";
+import { PlaceholderProps } from "react-select/src/components/Placeholder";
+import { SingleValueProps } from "react-select/src/components/SingleValue";
+import { ValueType } from "react-select/src/types";
+import { Omit } from "@material-ui/types";
+
+interface OptionType {
+  label: string;
+  value: string;
+}
+
+const suggestions: OptionType[] = [
+  { label: "Fashion" },
+  { label: "Oil And Gas" },
+  { label: "Software" },
+  { label: "Agriculture" },
+  { label: "Urban development" }
+].map(suggestion => ({
+  value: suggestion.label,
+  label: suggestion.label
+}));
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      minWidth: "100%",
+      marginTop: "0.85em"
+      // position: 'static'
+    },
+    input: {
+      display: "flex",
+      padding: 0,
+      height: "auto"
+    },
+    valueContainer: {
+      display: "flex",
+      flexWrap: "wrap",
+      flex: 1,
+      alignItems: "center",
+      overflow: "hidden",
+      padding: "0.8em"
+    },
+    chip: {
+      margin: theme.spacing(0.5, 0.25)
+    },
+    chipFocused: {
+      backgroundColor: emphasize(
+        theme.palette.type === "light"
+          ? theme.palette.grey[300]
+          : theme.palette.grey[700],
+        0.08
+      )
+    },
+    noOptionsMessage: {
+      padding: theme.spacing(1, 2)
+    },
+    singleValue: {
+      fontSize: 16
+    },
+    placeholder: {
+      position: "absolute",
+      left: 10,
+      bottom: 12,
+      fontSize: 16
+    },
+    paper: {
+      position: "absolute",
+      zIndex: 3,
+      marginTop: theme.spacing(1),
+      left: 0,
+      right: 0,
+      height: "30vh",
+      overflow: "hidden"
+    },
+    divider: {
+      height: theme.spacing(2)
+    }
+  })
+);
+
+function NoOptionsMessage(props: NoticeProps<OptionType>) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+type InputComponentProps = Pick<BaseTextFieldProps, "inputRef"> &
+  HTMLAttributes<HTMLDivElement>;
+
+function inputComponent({ inputRef, ...props }: InputComponentProps) {
+  return <div ref={inputRef} {...props} />;
+}
+
+function Control(props: ControlProps<OptionType>) {
+  const {
+    children,
+    innerProps,
+    innerRef,
+    selectProps: { classes, TextFieldProps }
+  } = props;
+
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: classes.input,
+          ref: innerRef,
+          children,
+          ...innerProps
+        }
+      }}
+      {...TextFieldProps}
+    />
+  );
+}
+
+function Option(props: OptionProps<OptionType>) {
+  return (
+    <MenuItem
+      ref={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+type MuiPlaceholderProps = Omit<PlaceholderProps<OptionType>, "innerProps"> &
+  Partial<Pick<PlaceholderProps<OptionType>, "innerProps">>;
+function Placeholder(props: MuiPlaceholderProps) {
+  const { selectProps, innerProps = {}, children } = props;
+  return (
+    <Typography
+      color="textSecondary"
+      className={selectProps.classes.placeholder}
+      {...innerProps}
+    >
+      {children}
+    </Typography>
+  );
+}
+
+function SingleValue(props: SingleValueProps<OptionType>) {
+  return (
+    <Typography
+      className={props.selectProps.classes.singleValue}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function ValueContainer(props: ValueContainerProps<OptionType>) {
+  return (
+    <div className={props.selectProps.classes.valueContainer}>
+      {props.children}
+    </div>
+  );
+}
+
+function MultiValue(props: MultiValueProps<OptionType>) {
+  return (
+    <Chip
+      tabIndex={-1}
+      label={props.children}
+      className={clsx(props.selectProps.classes.chip, {
+        [props.selectProps.classes.chipFocused]: props.isFocused
+      })}
+      onDelete={props.removeProps.onClick}
+      deleteIcon={<CancelIcon {...props.removeProps} />}
+    />
+  );
+}
+
+function Menu(props: MenuProps<OptionType>) {
+  return (
+    <Paper
+      square
+      className={props.selectProps.classes.paper}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Paper>
+  );
+}
+
+const components = {
+  Control,
+  Menu,
+  MultiValue,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer
+};
+
+export default function IntegrationReactSelect(props: any) {
+  const { input, style } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [multi, setMulti] = React.useState<ValueType<OptionType>>(
+    input.value ? input.value : null
+  );
+
+  const handleChangeMulti = (value: ValueType<OptionType>) => {
+    setMulti(value);
+    input.onChange(value);
+  };
+
+  const selectStyles = {
+    input: (base: CSSProperties) => ({
+      ...base,
+      color: theme.palette.text.primary,
+      "& input": {
+        font: "inherit"
+      }
+    })
+  };
+
+  return (
+    <div className={classes.root}>
+      <NoSsr>
+        <Select
+          classes={classes}
+          styles={selectStyles}
+          inputId="react-select-multiple"
+          TextFieldProps={{
+            InputLabelProps: {
+              htmlFor: "react-select-multiple",
+              shrink: true
+            },
+
+            variant: "outlined"
+          }}
+          placeholder="Categories"
+          options={suggestions}
+          components={components}
+          value={multi}
+          onChange={handleChangeMulti}
+          isMulti
+          name={input.name}
+        />
+      </NoSsr>
+    </div>
+  );
+}
